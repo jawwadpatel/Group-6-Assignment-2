c62024067b97d296aa1e1f115674a8d0e919ecc6
Jawwad Patel <jawwadmemon89@gmail.com>
2016-10-06 13:22:14 +1100

    Final Files

---
+++ src/library/interfaces/entities/ELoanState.java
@@ -0,0 +1,16 @@
+/*
+ * Decompiled with CFR 0_115.
+ */
+package library.interfaces.entities;
+
+public enum ELoanState {
+    PENDING,
+    CURRENT,
+    OVERDUE,
+    COMPLETE;
+    
+
+//    private ELoanState(String string2, int n2) {
+//    }
+}
+
---
+++ src/library/interfaces/entities/EMemberState.java
@@ -0,0 +1,14 @@
+/*
+ * Decompiled with CFR 0_115.
+ */
+package library.interfaces.entities;
+
+public enum EMemberState {
+    BORROWING_ALLOWED,
+    BORROWING_DISALLOWED;
+    
+//
+//    private EMemberState(String string2, int n2) {
+//    }
+}
+
